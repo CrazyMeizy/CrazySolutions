6b91583933065277a243ac7c441cfc11b4ff7451
Chuprov Dmitriy <dimache@niuitmo.ru>
2025-05-20 23:19:46 +0300

    Update HttpApplication.scala
    minor

--- scala/HTTP Application/src/main/scala/HttpApplication.scala
+++ scala/HTTP Application/src/main/scala/HttpApplication.scala
@@ -13,33 +13,28 @@ object HttpApplication extends IOApp {
     def application: Resource[IO, HttpServerCats] = for {
       _ <- Resource.make(IO.println("Starting application..."))(_ => IO.println("Application stopped"))
 
-      // load configuration
       config  <- AppConfiguration.unsafeLoad[IO](ConfigSource.default).toResource
       backend <- HttpClientCatsBackend.resource[IO]()
 
       crawlerClient = CrawlerClient.make(backend)
 
-      // create services
       authService   <- AuthService.make[Resource[IO, *], IO]
       animalService <- AnimalService.make[Resource[IO, *], IO]
       _ = CrawlerService.make(crawlerClient)
       _ <- AnimalService.populate(animalService).toResource
       _ <- AuthService.populate(authService).toResource
 
-      // create controllers
       animalApi = AnimalApi.make[IO](animalService, authService)
       authApi   = RegisterApi.make[IO](authService)
 
-      // start server
       httpServer <- HttpServerCats.startServer[IO](List(animalApi, authApi), config.server)
 
       _ <- Resource.make(IO.println("Application started"))(_ => IO.println("Closing application..."))
     } yield httpServer
 
-    // run application
     application.useForever
       .as(ExitCode.Success)
-      .handleErrorWith { exception => // handle all errors
+      .handleErrorWith { exception => 
         IO.println(s"Error: ${exception.getMessage}") >>
           IO.println("Application exited with error") >>
           IO.pure(ExitCode.Error)
